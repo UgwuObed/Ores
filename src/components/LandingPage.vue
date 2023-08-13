0bacc0728a639c7ea789c927d02edbe0bf187f81
Ugwu Obed <vjazzyvictor@gmail.com>
2023-08-13 23:11:48 +0100

    fest

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="landing-page">
+        <div class="navigation">
         <div class="logo">
             <img src="@/assets/logo1.png" alt="Your Logo">
           </div>
@@ -12,6 +13,7 @@
               <span class="ore-letter">s</span>
             </p>
           </div>
+    
           <div class="navbar">
             <div class="search-bar">
                 <input type="text" placeholder="Search Recipe..">
@@ -21,7 +23,20 @@
           <div class="nav-links">
             <router-link to="/about" class="nav-link">About Us</router-link>
             <span class="link-space"></span>
-            <router-link to="/contact" class="nav-link">Contact Us</router-link>
+            <router-link to="/contact" class="nav-link1">Contact</router-link>
+              </div>
+              <div class="user-buttons">
+                <button class="signup-button">Sign Up</button>
+                <div class="button-space"></div>
+                <button class="login-button">Login</button>
+              </div>
+            </div>
+              <div class="center-left">
+                <h2 class="ore-text">
+                  <span class="ore-highlight">Ore’s</span><br />
+                  Health Place
+                </h2>
+                <p class="descrip">Get fresh and tasty continental and intercontinental <br>recipes and how to make them, check and calculate <br>their <span class="calo">calorie</span> count </p>
               </div>
       <router-view />
       
